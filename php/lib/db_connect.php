b8a6960f990f5dcff73dc7b1e1d69e366954f993
Jeremy Duke <jeremy@tetrapod.io>
2016-05-02 15:40:19 -0700

    Git Ignore db_connect.php (again).

--- php/lib/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /* Update the credentials below as necessary.  Updates will not appear in your git repo changelog */
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "123e123e";
-    $dbname = "prism";
-?>
