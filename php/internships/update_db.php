3901de5a41399df3e744e731c5fc05d4e1c89ecd
GelLiNN <knealy@splunk.com>
2016-05-02 19:58:01 -0700

    changed add internship to student teams logic

--- php/internships/update_db.php
+++ php/internships/update_db.php
@@ -2,6 +2,7 @@
 # update_db php file: db write-access layer
 # Author: Tim Davis
 # Author: Kellan Nealy
+include "query_db.php";
 
 # takes in an associative array of all the column values for an internship
 # uses INSERT INTO to add to the DB
@@ -9,10 +10,37 @@ function add_internship($internship_data) {
 	$conn = db_connect();
 
 	# get the max column value for InternshipID so we can correctly add to the table
-	$cur_max_id = mysqli_fetch_assoc(mysqli_query($conn, "SELECT MAX(InternshipId) FROM internships;"));
-	$new_id = $cur_max_id['InternshipId'] + 1;
-	$internship_data['InternshipId'] = $new_id;
+	$new_id = mysqli_insert_id($conn);
 
+	# generate last updated datetime
+	$lastUpdated = date('Y-m-d H:i:s');
+
+	$queryFormat = "insert internships(
+		InternshipId,
+		PositionTitle,
+		Description,
+		OrganizationId,
+		DatePosted,
+		StartDate,
+		EndDate,
+		SlotsAvailable,
+		LastUpdated,
+		ExpirationDate)
+		values ('%d', '%s', '%s', '%d', '%s', '%s', '%s', '%d', '%s', '%s');";
+
+	$sql = sprintf($queryFormat,
+		$new_id,
+		$internship_data['PositionTitle'],
+		$internship_data['Description'],
+		$internship_data['OrganizationId'],
+		$internship_data['DatePosted'],
+		$internship_data['StartDate'],
+		$internship_data['EndDate'],
+		$internship_data['SlotsAvailable'],
+		$lastUpdated,
+		$internship_data['ExpirationDate']);
+
+	/*
 	# column names in first line of query are unnecessary if we want to take those away
 	$sql = "INSERT INTO `internships`
 		(`InternshipId`, `PositionTitle`, `Description`, `OrganizationId`, `DatePosted`,
@@ -21,6 +49,7 @@ function add_internship($internship_data) {
 			$internship_data['OrganizationId'],[$internship_data['DatePosted'],$internship_data['StartDate'],
 			$internship_data['EndDate'],$internship_data['SlotsAvailable'],$internship_data['LastUpdated'],
 			$internship_data['ExpirationDate']);"
+	*/
 
 	$result = mysqli_query($conn, $sql);
 	if (!$result) {
