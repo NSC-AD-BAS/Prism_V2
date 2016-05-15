089a17e488d11cc0f73ecd8a7196ddaebcda8bb5
Jeremy Duke <jeremy@tetrapod.io>
2016-05-15 03:17:45 -0700

    Add error page to help with input validation failure debugging.

---
+++ php/companies/error.php
@@ -0,0 +1,20 @@
+<?php
+
+//Includes
+include "page_builder.php";
+include "query_db.php";
+
+//TODO: Better Translation and/or method of passing of error codes.
+render_header('Error', false);
+render_nav('Something has gone terribly wrong!  :( ');
+
+$out = '
+    <p class="alert">' . urldecode($_SERVER['QUERY_STRING']) . '</p>
+    <hr>
+    <a class="button" href="list.php"><div>OK</div></a>
+';
+
+echo $out;
+
+render_footer();
+?>
