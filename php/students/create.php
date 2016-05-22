0d61839099308a9ecb199aa38565cccda45ee661
Nathan Flint <nlflint@gmail.com>
2016-05-21 20:08:56 -0700

    Converted create page to use Companies styling

--- php/students/create.php
+++ php/students/create.php
@@ -18,98 +18,107 @@
     render_nav("Create Student");
 
 ?>
-    <form action="create_post.php" method="post">
-        <table id="internship_detail">
-            <tr>
-                <th>First</th>
-                <td><input name="student[first]" type="text" required></td>
-            </tr>
-            <tr>
-                <th>Middle</th>
-                <td><input name="student[middle]" type="text"></td>
-            </tr>
-            <tr>
-                <th>Last</th>
-                <td><input name="student[last]" type="text" required></td>
-            </tr>
-            <tr>
-                <th>Preferred Name</th>
-                <td><input name="student[preferred]"type="text"></td>
-            </tr>
-            <tr>
-                <th>SSID</th>
-                <td><input name="student[ssid]" type="text" required></td>
-            </tr>
-            <tr>
-                <th>Cohort/Year</th>
-                <td><input name="student[cohort]" type="text" required></td>
-            </tr>
-            <tr>
-                <th>Program Status</th>
-                <td>
-                    <select name="student[program_status_id]">
-                        <?=$programStatusesOptions?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <th>Internship/Capstone Status</th>
-                <td>
-                    <select name="student[internship_capstone_status_id]">
-                       <?=$internCapstoneStatusesOptions?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <th>Internship</th>
-                <td><input name="student[internship]" type="text"></td>
-            </tr>
-            <tr>
-                <th>Application Status</th>
-                <td>
-                    <select name="student[application_status_id]">
-                        <?=$applicationStatusesOptions?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <th>Resume</th>
-                <td><input name="student[resume_url]" type="text"></td>
-            </tr>
-            <tr>
-                <th>LinkedIn Profile</th>
-                <td><input name="student[linked_in_url]" type="text"></td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td><input name="student[email]"></td>
-            </tr>
-            <tr>
-                <th>Phone</th>
-                <td><input name="student[phone]"></td>
-            </tr>
-            <tr>
-                <th>Address Line 1</th>
-                <td><input name="student[address1]" type="text"></td>
-            </tr>
-            <tr>
-                <th>Address Line 2</th>
-                <td><input name="student[address2]" type="text"></td>
-            </tr>
-            <tr>
-                <th>City</th>
-                <td><input name="student[city]" type="text"></td>
-            </tr>
-            <tr>
-                <th>State</th>
-                <td><input name="student[state]" type="text"></td>
-            </tr>
-            <tr>
-                <th>Zipcode</th>
-                <td><input name="student[zipcode]" type="text"></td>
-            </tr>
-            <tr><td><input type="submit" value="Create Student"></td></tr>
-        </table>
-    </form>
-    <a class="button" href="list.php"><div>Back to List</div></a>
+    <div class="wrapper">
+        <div class="detail_table">
+            <form action="create_post.php" method="post">
+                <table>
+                    <tr>
+                        <td>First</td>
+                        <td><input class="textbox" name="student[first]" type="text" required></td>
+                    </tr>
+                    <tr>
+                        <td>Middle</td>
+                        <td><input class="textbox" name="student[middle]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Last</td>
+                        <td><input class="textbox" name="student[last]" type="text" required></td>
+                    </tr>
+                    <tr>
+                        <td>Preferred Name</td>
+                        <td><input class="textbox" name="student[preferred]"type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>SSID</td>
+                        <td><input class="textbox" name="student[ssid]" type="text" required></td>
+                    </tr>
+                    <tr>
+                        <td>Cohort/Year</td>
+                        <td><input class="textbox" name="student[cohort]" type="text" required></td>
+                    </tr>
+                    <tr>
+                        <td>Program Status</td>
+                        <td>
+                            <select class="textbox" name="student[program_status_id]">
+                                <?=$programStatusesOptions?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Internship/Capstone Status</td>
+                        <td>
+                            <select class="textbox" name="student[internship_capstone_status_id]">
+                               <?=$internCapstoneStatusesOptions?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Internship</td>
+                        <td><input class="textbox" name="student[internship]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Application Status</td>
+                        <td>
+                            <select class="textbox" name="student[application_status_id]">
+                                <?=$applicationStatusesOptions?>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Resume</td>
+                        <td><input class="textbox" name="student[resume_url]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>LinkedIn Profile</td>
+                        <td><input class="textbox" name="student[linked_in_url]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input class="textbox" name="student[email]"></td>
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td><input class="textbox" name="student[phone]"></td>
+                    </tr>
+                    <tr>
+                        <td>Address Line 1</td>
+                        <td><input class="textbox" name="student[address1]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Address Line 2</td>
+                        <td><input class="textbox" name="student[address2]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>City</td>
+                        <td><input class="textbox" name="student[city]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>State</td>
+                        <td><input class="textbox" name="student[state]" type="text"></td>
+                    </tr>
+                    <tr>
+                        <td>Zipcode</td>
+                        <td><input class="textbox" name="student[zipcode]" type="text"></td>
+                    </tr>
+                    <hr>
+                </table>
+                <hr>
+                <div class="lower_nav">
+                    <input class="form_button" type="submit" value="Create Student">
+                    <a class="button" href="list.php"><div>Student List</div></a>
+                </div>
+            </form>
+        </div>
+    </div>
+    
 <?php render_footer(); ?>
