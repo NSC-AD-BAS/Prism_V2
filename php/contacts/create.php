2c95abf6f70a7f53afc1c2a242055a3762d0fff0
Jeremy Duke <jeremy@tetrapod.io>
2016-05-24 02:39:33 -0700

    Change /admin/list.php to /users/list.php where I found it.

--- php/contacts/create.php
+++ php/contacts/create.php
@@ -51,7 +51,7 @@
         <li class="left"><a href="../internships/list.php">Internships</a></li>
         <li class="left"><a href="../companies/list.php">Companies</a></li>
         <li class="left"><a href="../students/list.php">Students</a></li>
-        <li class="left"><a href="../admin/list.php">Users</a></li>
+        <li class="left"><a href="../users/list.php">Users</a></li>
         <li class="right"><a href="list.php">Logout</a></li>
         <li class="right"><a href="list.php">Profile</a></li>
         <li id="welcome" class="right">Welcome, You!</li>
--- php/users/includes/header.php
+++ php/users/includes/header.php
@@ -46,7 +46,7 @@
                 <li class="left"><a href="../internships/list.php">Internships</a></li>
                 <li class="left"><a href="../companies/list.php">Companies</a></li>
                 <li class="left"><a href="../students/list.php">Students</a></li>
-                <li class="left"><a href="../admin/list.php">Users</a></li>
+                <li class="left"><a href="../users/list.php">Users</a></li>
                 <li class="left"><a href="#">Change Log</a></li>
 
                 <li class="right"><a href="../login/logout.php">Logout</a></li>
