a8440b851bea88cc903fe5a6b0a857eb82dfe73a
Jeremy Duke <jeremy@tetrapod.io>
2016-05-24 00:39:35 -0700

    Organize and comment query_db and update_db, bring over all of the Internships db functions.

--- php/db/query_db.php
+++ php/db/query_db.php
@@ -7,6 +7,10 @@
     TODO: SECURITY - Move all DB function files outside of webroot to prevent direct access
 */
 
+/*
+    Handy DB Functions for everyone!
+*/
+
 //Stand-up and return a connect object
 function db_connect() {
     include '../lib/db_connect.php';
@@ -18,6 +22,14 @@ function db_connect() {
     return $conn;
 }
 
+//Determine if logged-in user is an admin.  See the fixer, needs improvement
+function isAdmin() {
+    //FIXME: This needs to (eventually) evaluate that the user is both logged in *and* has admin credentials.
+    //Change to false to see nav and detail buttons auto-magically disappear.
+    return true;
+}
+
+//Get the result set from an arbitrary view name
 function get_view_data($view) {
     $conn = db_connect();
     $sql  = "SELECT * FROM $view";
@@ -31,6 +43,11 @@ function get_view_data($view) {
     return $output;
 }
 
+/*
+    Organization Specific DB Queries
+*/
+
+//Get the data from some view where OrgID = some ID.
 function get_view_data_where($view, $orgId) {
     $conn = db_connect();
     $sql  = "SELECT * FROM $view WHERE OrganizationId = $orgId";
@@ -44,34 +61,72 @@ function get_view_data_where($view, $orgId) {
     return $output;
 }
 
+//Return the list of all companies
 function get_companies_list() {
     return get_view_data('org_list');
 }
 
+//Get the org detail for an org
 function get_company_detail($id) {
     return get_view_data_where('org_detail', $id);
 }
 
+//Get the internships for an org
 function get_internships_by_company($id) {
     return get_view_data_where('internships', $id);
 }
 
+//Get the contacts for an org
 function get_contacts_by_company($id) {
     return get_view_data_where('org_contact_list', $id);
 }
 
+//Get all deleted companies
 function get_deleted_companies() {
     return get_view_data('org_list_archived');
 }
 
+//Get the last error and return it.
 function get_last_error($conn) {
     return mysqli_error($conn);
 }
 
-function isAdmin() {
-    //FIXME: This needs to (eventually) evaluate that the user is both logged in *and* has admin credentials.
-    //Change to false to see nav and detail buttons auto-magically disappear.
-    return true;
+/*
+    Internship Specific DB Queries
+*/
+
+// Returns internship list result
+function get_internship_list() {
+    $conn = db_connect();
+    $sql = "SELECT DISTINCT *
+            FROM internship_list;";
+    $result = mysqli_query($conn, $sql);
+    while ($row = $result->fetch_assoc()) {
+        $output[] = $row;
+    }
+    //clean-up result set and connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+    return $output;
+}
+
+// Returns intership detail result
+function get_internship_detail($id) {
+    $conn = db_connect();
+    $sql = "SELECT DISTINCT *
+            FROM internship_detail
+            WHERE InternshipId = $id
+            LIMIT 1;";
+    $result = mysqli_query($conn, $sql);
+    while ($row = $result->fetch_assoc()) {
+        $output[] = $row;
+    }
+    //clean-up result set and connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+    return $output;
 }
 
 ?>
--- php/db/update_db.php
+++ php/db/update_db.php
@@ -4,10 +4,14 @@
     update_db.php - Runs update queries against database.
     includes: none
     included by: Any page that creates, edits or deletes records in the DB
-    TODO: SECURITY - Move all DB function files outside of webroot to prevent direct access
+    TODO: SECURITY - Move all DB write functions outside of webroot to prevent direct access
 */
 
-//TODO: Input validation at calling function
+/*
+    Organization Specific DB Queries
+*/
+
+//Create a company and return the orgId
 function create_company($query) {
     $conn = db_connect();
     mysqli_query($conn, $query);
@@ -15,7 +19,7 @@ function create_company($query) {
     return $orgId;
 }
 
-//TODO: Handle return statements properly in calling code
+//Update the DB with the given query
 function update_db($query) {
     $conn = db_connect();
     $result = true;
@@ -26,12 +30,131 @@ function update_db($query) {
     return $result;
 }
 
+//Create an internship at company create time.  FIXME: This shouldn't be necessary when we get the view updated
 function create_internship_for_company($query) {
     return update_db($query);
 }
 
+//Nicely named stub.
 function update_company($query) {
     return update_db($query);
+}
+
+/*
+    Internship Specific DB Queries
+*/
+
+// TODO: do not show internships that are past their expiration date
+
+// Creates an internship given the data
+function add_internship($internship_data) {
+	$conn = db_connect();
+
+	// get the max column value for InternshipID so we can correctly add to the table
+	$new_id = mysqli_insert_id($conn);
+
+	// generate last updated datetime
+	$lastUpdated = date('Y-m-d H:i:s');
+
+    //TODO: Move to query_builder.
+	$queryFormat = "INSERT internships(
+		InternshipId,
+		PositionTitle,
+		Description,
+		OrganizationId,
+		DatePosted,
+		StartDate,
+		EndDate,
+		LastUpdated,
+		ExpirationDate)
+		values ('%d', '%s', '%s', '%d', '%s', '%s', '%s', '%s', '%s');";
+
+	$sql = sprintf($queryFormat,
+		$new_id,
+		$internship_data['PositionTitle'],
+		$internship_data['Description'],
+		$internship_data['OrganizationId'],
+		$internship_data['DatePosted'],
+		$internship_data['StartDate'],
+		$internship_data['EndDate'],
+		$lastUpdated,
+		$internship_data['ExpirationDate']);
+
+	$result = mysqli_query($conn, $sql);
+
+	//TODO: return the result to calling script and pass error to error page.
+	if (!$result) {
+		echo "INSERT INTO internships table failed";
+	}
+}
+
+// Updates an internship given the id and replacement data
+function update_internship($internship_data, $intId) {
+	$conn = db_connect();
+
+	// generate last updated datetime
+	$lastUpdated = date('Y-m-d H:i:s');
+
+    //TODO: Move to query_builder.
+	$queryFormat = "REPLACE internships(
+		InternshipId,
+		PositionTitle,
+		Description,
+		OrganizationId,
+		DatePosted,
+		StartDate,
+		EndDate,
+		LastUpdated,
+		ExpirationDate)
+		values ('%d', '%s', '%s', '%d', '%s', '%s', '%s', '%s', '%s');";
+
+	$sql = sprintf($queryFormat,
+		$intId,
+		$internship_data['PositionTitle'],
+		$internship_data['Description'],
+		$internship_data['OrganizationId'],
+		$internship_data['DatePosted'],
+		$internship_data['StartDate'],
+		$internship_data['EndDate'],
+		$lastUpdated,
+		$internship_data['ExpirationDate']);
+
+	$result = mysqli_query($conn, $sql);
+
+	//TODO: return the result to calling script and pass error to error page.
+	if (!$result) {
+	    echo $result;
+		echo "INSERT INTO internships table failed";
+	}
+}
+
+// Deletes an internship given the id
+function delete_internship($intId) {
+	$delete = 1; // 1 = deleted
+	delete_undelete_internship($intId, $delete);
+}
+// Undeletes an internship given the id
+function undelete_internship($intId) {
+	$delete = 0; // 0 = not deleted
+	delete_undelete_internship($intId, $delete);
+}
+//endTODO
+
+// Helper function for deleting and undeleting an internship
+// Deletes or undeletes an internship given the id and delete value
+function delete_undelete_internship($intId, $delete_undelete) {
+	$conn = db_connect();
+
+	$sql = "UPDATE internships
+				SET isDeleted = " . $delete_undelete . "
+				WHERE InternshipId = " . $intId;
+
+	$result = mysqli_query($conn, $sql);
+
+	//TODO: return the result to calling script and pass error to error page.
+	if (!$result) {
+		echo "UPDATE internships table failed";
+	}
 }
 
 ?>
