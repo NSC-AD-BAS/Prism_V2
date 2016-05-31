f5a8337ae95d0bad64e830ccc73fa6df591ba206
Nathan Flint <nlflint@gmail.com>
2016-05-30 21:25:17 -0700

    Refactored company list before working on search.

--- php/companies/list.php
+++ php/companies/list.php
@@ -7,44 +7,28 @@ include "../render/render_company.php";
 include "../db/query_db.php";
 
 //Toggle showing deleted / archived companies
-if (isset($_GET['archived']) && $_GET['archived'] == "true") {
-    $archived = true;
-} else {
-    $archived = false;
-}
+$archived = isset($_GET['archived']) && $_GET['archived'];
 
 //Allow column sorting
-if (isset($_GET['sort'])) {
-    $sort = $_GET['sort'];
-} else {
-    $sort = "";
+function getWithDefault($key, $defaultValue) {
+    return isset($_GET[$key]) ? $_GET[$key] : $defaultValue;
 }
-if (isset($_GET['order'])) {
-    $order = $_GET['order'];
-} else {
-    $order = "ASC";
-}
+$sort = getWithDefault('sort', "");
+$order = getWithDefault('order', "ASC");
 
-//Get ugly view names from nicer url args
-if ($sort == "comp") {
-    $field = "`Organization Name`";
-} else if ($sort == "loc") {
-    $field = "Location";
-} else if ($sort == "avail") {
-    $field = "`Available Internships`";
-} else {
-    //someone passed in something they oughtn't have, default to company alpha sort
-    $field = "`Organization Name`";
-}
+$urlArgsToNameLookup = array(
+    "comp" => "`Organization Name`",
+    "loc" => "Location",
+    "avail" => "`Available Internships`"
+);
+$field = array_key_exists($sort, $urlArgsToNameLookup) ? $urlArgsToNameLookup[$sort] : "`Organization Name`";
+
+$data = $archived ? get_deleted_companies() : get_companies_list_sorted($field, $order);
 
 //Render the default page
 render_header('Companies', false);
 render_nav('Company List');
-if (!$archived) {
-    renderCompanyList(get_companies_list_sorted($field, $order), false);
-} else {
-    renderCompanyList(get_deleted_companies(), true);
-}
+renderCompanyList($data, $archived);
 render_footer();
 
 ?>
