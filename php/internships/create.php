e75df5b2f14fae5d5935f1fedda5b9092b858aed
timdavish <timdavish@gmail.com>
2016-06-06 19:15:58 -0700

    Added placeholder values to create page

--- php/internships/create.php
+++ php/internships/create.php
@@ -17,15 +17,15 @@ include("../render/page_builder.php");
 
 # process the POST params for create new internship
 function try_post_process() {
-	if (isset($_POST['PositionTitle']) && isset($_POST['OrganizationId']) && isset($_POST['DatePosted'])
-		&& isset($_POST['StartDate']) && isset($_POST['EndDate']) && isset($_POST['Location'])
-		&& isset($_POST['ExpirationDate']) && isset($_POST['Description'])) {
+	if (isset($_POST['PositionTitle']) && isset($_POST['OrganizationId'])
+		&& isset($_POST['DatePosted']) && isset($_POST['StartDate'])
+		&& isset($_POST['EndDate']) && isset($_POST['ExpirationDate'])
+		&& isset($_POST['Description'])) {
 
 		# need to follow up with DB people about how to setup link between org and internship
 		$internship_data = array("PositionTitle"=>$_POST['PositionTitle'], "OrganizationId"=>$_POST['OrganizationId'],
 			"DatePosted"=>$_POST['DatePosted'], "StartDate"=>$_POST['StartDate'], "EndDate"=>$_POST['EndDate'],
-			"Location"=>$_POST['Location'],	"ExpirationDate"=>$_POST['ExpirationDate'],
-			"Description"=>$_POST['Description']);
+			"ExpirationDate"=>$_POST['ExpirationDate'], "Description"=>$_POST['Description']);
 
 		add_internship($internship_data);
 		header("Location: list.php");
@@ -34,12 +34,13 @@ function try_post_process() {
 
 # Prints the main html for this internship createS
 function print_create_main() { ?>
+
     <h1>Internship Create</h1>
     <form action="create.php" method="POST">
         <table id="internship_create_edit">
             <tr>
                 <th>Position</th>
-                <td><input name="PositionTitle" type="text" /></td>
+                <td><input name="PositionTitle" type="text" placeholder="Position" /></td>
             </tr>
             <tr>
                 <th>Company</th>
@@ -53,27 +54,23 @@ function print_create_main() { ?>
             </tr>
             <tr>
                 <th>Date Posted</th>
-                <td><input name="DatePosted" type="text" /></td>
+                <td><input name="DatePosted" type="text" placeholder="yyyy-mm-dd" /></td>
             </tr>
             <tr>
                 <th>Start Date</th>
-                <td><input name="StartDate" type="text" /></td>
+                <td><input name="StartDate" type="text" placeholder="yyyy-mm-dd" /></td>
             </tr>
             <tr>
                 <th>End Date</th>
-                <td><input name="EndDate" type="text" /></td>
+                <td><input name="EndDate" type="text" placeholder="yyyy-mm-dd" /></td>
 			</tr>
             <tr>
-                <th>Location</th>
-                <td><input name="Location" type="text" /></td>
-            </tr>
-            <tr>
                 <th>Expiration Date</th>
-                <td><input name="ExpirationDate" type="text" /></td>
+                <td><input name="ExpirationDate" type="text" placeholder="yyyy-mm-dd" /></td>
             </tr>
             <tr>
                 <th>Description</th>
-                <td><textarea rows="3" name="Description"></textarea>
+                <td><textarea rows="3" name="Description" placeholder="Enter a description" ></textarea>
                 </td>
             </tr>
         </table>
