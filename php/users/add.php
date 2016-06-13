cb2793d591fd0262feb4a8a802a1350e8c7de1db
Joe McLaughlin <jomclis@gmail.com>
2016-06-13 01:57:04 -0700

    Cleans code's formatting

--- php/users/add.php
+++ php/users/add.php
@@ -2,26 +2,20 @@
 <br><br>        
 <?php 
 /**
-  * Displays newly created user and its fields
+  * Displays form to create a new user
   */
-	//AddPage
-	//*
- 	echo "<form action='prism-input.php?id=".$myID."' method='post'>";
-	echo "First Name: <input type='text' name='firstname' ><br>";
-	echo "Last Name: <input type='text' name='lastName' ><br>";
-	//echo "Contact: <input type='text' name='contact' ><br>";
-    echo "Phone #: <input type='text' name='userPhone' ><br>";
-	echo "E-mail : <input type='text' name='userEmail' ><br>";
-    
-	echo "
+ 	echo "<form action='prism-input.php?id=".$myID."' method='post'>
+	First Name: <input type='text' name='firstname' ><br>
+	Last Name: <input type='text' name='lastName' ><br>
+    Phone #: <input type='text' name='userPhone' ><br>
+	E-mail : <input type='text' name='userEmail' ><br>
     User Type: 
     <input type='radio' name='type' value='2'>Admin
     <input type='radio' name='type' value='1' checked>Student
     <input type='radio' name='type' value='3'>Faculty<br><br>
-    ";
-	echo "<input type='submit' name='add'>";
-	echo "<input type='hidden' name='act' value='add' />";
-	echo "</form>";
+	<input type='submit' name='add'>
+	<input type='hidden' name='act' value='add' />
+	</form>";
 ?>
 <br><br>
 <a href=list.php> Back to list</a>
