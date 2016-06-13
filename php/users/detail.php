ffb0d2ecc7c12dff421d187355dbd65bb484a537
Joe McLaughlin <jomclis@gmail.com>
2016-06-13 02:02:22 -0700

    Cleans code's formatting

--- php/users/detail.php
+++ php/users/detail.php
@@ -1,24 +1,23 @@
-<?php 
-include 'includes/header.php';
-?>
+<?php include 'includes/header.php';?>
 <br><br>        
-<?php 
-//include 'includes/functionsinc.php';
+<?php
 /**
   * Retrieves and displays details regarding a specified account.
   * Information includes: Name, phone number, email, and user type
   */
-	if(isset($_GET['id']) && (int)$_GET['id'] > 0){ //proper data must be on querystring
-	 	$myID = (int)$_GET['id']; //Convert to integer, will equate to zero if fails
+
+    #pulls users ID
+	if(isset($_GET['id']) && (int)$_GET['id'] > 0){ #proper data must be on querystring
+	 	$myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
 	}
 	$data = get_user_detail($myID);
 
-	//Case: no results
+	#Case: no results
     if (empty($data)) {
         echo "0 results";
     }
 
-    //assign variables for readability
+    #assign variables for readability
     foreach ($data as $d) {
         $userName = dbOut($d['Name']);
         $userPhone = dbOut($d['Phone']);
@@ -26,17 +25,14 @@ include 'includes/header.php';
         $userType = dbOut($d['User Type']);
         $usrId = dbOut($d['User ID']);
     }
-    
-    //TODO: Make this a proper detail table
-	echo "<br /><br />Name : " . $userName . "<br>";
-	echo "Phone #: " . $userPhone . "<br>";
-	echo "E-mail : " . $userEmail . "<br>";
-	echo "Type : " . $userType . "<br>" . "<br>";
-    
 
-    echo '&emsp;&emsp;&emsp; <a class="button" href=edit.php?id=' . $usrId . '><div>Edit</div></a>' . '&emsp;&emsp;&emsp;' . '<a class="button" href=delete.php><div>Delete</div></a>';
-    
-	
+    #TODO: Make this a proper detail table
+	echo "
+    <br /><br />Name : " . $userName . "<br>
+	Phone #: " . $userPhone . "<br>
+	E-mail : " . $userEmail . "<br>
+	Type : " . $userType . "<br><br>
+    &emsp;&emsp;&emsp; <a class='button' href=edit.php?id=" . $usrId . "><div>Edit</div></a>";
 ?>
 <br><br>
 <a href=list.php> Back to list</a>
