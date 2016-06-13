2c225e749535d2a65401b273e86a4e240df0fa5b
Joe McLaughlin <jomclis@gmail.com>
2016-06-13 02:40:23 -0700

    Cleans code's formatting, adds todo's

--- php/users/prism-input.php
+++ php/users/prism-input.php
@@ -1,20 +1,18 @@
 <?php 
-	#require '../../../../../includes/creds.php';
-    #define('SITE_URL', "http://joesarchive.com/sandbox/Prism_V2/php/users/");
+
     require '../lib/db_connect.php';
     require '../db/query_db.php';
-//	define('SITE_URL', "http://prism.tekbot.net/users/");
     
 	# Read the value of 'action' whether it is passed via $_POST or $_GET with $_REQUEST
 	if(isset($_REQUEST['act'])){$myAction = (trim($_REQUEST['act']));}else{$myAction = "";}
 
 	switch ($myAction) 
-	{//check 'act' for type of process
+	{#check 'act' for type of process
 
 		case "update": # 1)user is updating a field
 			updateUser();
 			break;
-		case "add": # 1)user is updating a field
+		case "add": # 1)user is adding a field
 			addUser();
 			break;		
 		default:
@@ -22,11 +20,12 @@
 			header('Location: '.$newURL);
 	}
 
-	/////////////////////////////////////////////////////////////////
 /**
   * Updates a user's fields
   * Fields updated include: first name, last name, phone number, email, and type
+  * #TODO:Add missing table fields(middle name etc...)
   */
+
 function updateUser(){
 	$conn = db_connect();
 
@@ -43,23 +42,23 @@ function updateUser(){
 	$sql = "UPDATE users set firstName='%s',lastName='%s',PhoneNumber='%s',EmailAddress='%s',TypeId=%d WHERE UserId = " . $myID;
 
 	$sql = sprintf($sql,$firstName,$lastName, $userPhone,$userEmail, $type);
-	//var_dump($sql); //test sql query 
-    //die();
 	$result = mysqli_query($conn,$sql); 
+    
 	if ($result)
-	{//successful update!
+	{#successful update!
 		$newURL = 'detail.php?id=' . $myID;
 	}else{
 		$newURL = 'edit.php?id=' . $myID;
 	}
 	header('Location: '.$newURL);
-	//die();
+    die();
 }
 
 /**
   * Adds a new user to the database along with given fields
   * User fields include: first name, last name, phone number, and email
-*/
+  * #TODO:Add missing table fields(middle name etc...)
+  */
 function addUser(){
 	$conn = db_connect();
 
@@ -71,33 +70,23 @@ function addUser(){
     if(isset($_POST['lastName'])){$lastName=$_POST['lastName'];}else{$lastName = '';}
     if(isset($_POST['userPhone'])){$userPhone=$_POST['userPhone'];}else{$firstName = '';}
     if(isset($_POST['userEmail'])){$userEmail=$_POST['userEmail'];}else{$lastName = '';}
-    
-    //if(isset($_POST['contact'])){$contact=$_POST['contact'];}else{$contact = '';}
     if(isset($_POST['type'])){$type=$_POST['type'];}else{$type = '';}
     
-    
-    $firstLen = strlen($firstName);
-    $lastShort = substr($lastName,0,2);
-    
-    if($firstLen >=3)
+    if(strlen($firstName) >=3)#check if users last name is smaller than 3 characters
     { 
-        $userName = substr($firstName,0,3) . $lastShort;
-    }else{
-        $userName = $firstName . $lastShort;
+        $userName = substr($firstName,0,3) . substr($lastName,0,2);
+    }else{#if first name is shorter use as is for username
+        $userName = $firstName . substr($lastName,0,2);
     }
-    $userName = strtolower($userName);
-    $userPW = $userName;
+    $userName = strtolower($userName); #format users name to site standards
+    $userPW = $userName; #default pw is userName
     
 	$sql = "INSERT INTO users(FirstName,LastName,PhoneNumber,EmailAddress,TypeId,UserName,UserPassword) VALUES('%s','%s','%s','%s',%d,'%s','%s')";
 	$sql = sprintf($sql,$firstName,$lastName,$userPhone,$userEmail,$type,$userName,$userPW);
-	//var_dump($sql); //test sql query 
-	//die();
-
 	$result = mysqli_query($conn,$sql); 
-    //var_dump($result);
-    //die();
+
 	if ($result)
-	{//successful update!
+	{#successful update!
 		$newURL = 'list.php';
 	}else{
 		$newURL = 'add.php';
@@ -105,7 +94,4 @@ function addUser(){
 	header('Location: '.$newURL);
 	die();
 }
-/////////////////////////////////////////////////////////////////
-
-
 ?>
