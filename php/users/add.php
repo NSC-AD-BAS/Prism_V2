0cc08863ac20bb5ecc6b20b59b376ee589c3dab4
Joe McLaughlin <jomclis@gmail.com>
2016-06-13 03:38:01 -0700

    Fixes button layout and name for update user

--- php/users/add.php
+++ php/users/add.php
@@ -9,39 +9,47 @@
     "
         <form action='prism-input.php?id=".$myID."' method='post'>
         <div class='wrapper'>
-        <div class='detail_table'>
-            <table>
-                <tr>
-                    <td>First Name:</td>
-                    <td> <input type='text' name='firstname' ></td>
-                </tr>
-                
-            <tr>
-                <td>Last Name: </td>
-                <td><input type='text' name='lastName' ></td>
+            <div class='detail_table'>
+                <table>
+                    <tr>
+                        <td>First Name:</td>
+                        <td> <input type='text' name='firstname' ></td>
+                    </tr>
 
-            </tr>
-            <tr>
-                <td>Phone #: </td>
-                <td><input type='text' name='userPhone' ></td>
-            
-            </tr>
-            <tr>
-                <td>E-mail :</td> 
-                <td><input type='text' name='userEmail' ></td>
-            
-            </tr>
-            <tr>
-                <td>User Type: </td>
-                <td>
-                    <input type='radio' name='type' value='2'>Admin
-                    <input type='radio' name='type' value='1' checked>Student
-                    <input type='radio' name='type' value='3'>Faculty
-                </td>
-            </tr>
-            <input type='submit' name='add'>
-            <input type='hidden' name='act' value='add' />
-        <table>
+                    <tr>
+                        <td>Last Name: </td>
+                        <td><input type='text' name='lastName' ></td>
+
+                    </tr>
+                    <tr>
+                        <td>Phone #: </td>
+                        <td><input type='text' name='userPhone' ></td>
+
+                    </tr>
+                    <tr>
+                        <td>E-mail :</td> 
+                        <td><input type='text' name='userEmail' ></td>
+
+                    </tr>
+                    <tr>
+                        <td>User Type: </td>
+                        <td>
+                            <input type='radio' name='type' value='2'>Admin
+                            <input type='radio' name='type' value='1' checked>Student
+                            <input type='radio' name='type' value='3'>Faculty
+                        </td>
+                    </tr>
+                </table>
+                <hr>
+                <div class='lower_nav'>
+                    <div>
+                        <input class='button' type='submit' name='add' value='Update User'>
+                    </div>
+                </div> <!--end lower_nav -->
+            </div> <!--detail_table-->
+        </div> <!--wrapper-->
+        <input type='hidden' name='act' value='add' />
+        
         </form>
     ";
 ?>
