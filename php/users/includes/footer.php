48cd27fbdf254d26689b4866acd02fc0160fc821
Joe McLaughlin <jomclis@gmail.com>
2016-06-13 01:46:36 -0700

    Changes to use render

--- php/users/includes/footer.php
+++ php/users/includes/footer.php
@@ -1,7 +1,1 @@
-<!--The footer and end of all html for users-->
-        </main>
-        <footer>
-            <p><small>North Seattle College - PRISM &copy; <?=date('Y');?></small></p>
-        </footer>
-    </body>
-</html>
+<?php render_footer(); ?>
