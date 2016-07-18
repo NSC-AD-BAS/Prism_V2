f449cb880dcb758523c91400cb2a543ac51173ed
Joe McLaughlin <jomclis@gmail.com>
2016-07-17 23:49:54 -0700

    First stage of working pager implementation

--- php/users/includes/header.php
+++ php/users/includes/header.php
@@ -29,5 +29,44 @@
                 $header = "P R I S M - ";
         }
     
+/**
+ * Automatically loads classes by name when called.  This saves resources, as  
+ * only pages that need to reference the class actually load it.
+ *
+ * The autoload function allows us to name a file after the class, and customize it to 
+ * meet our needs, but load the file requiring the class by assuming the file name based on 
+ * the name of the class.  In our case, we'll add '.php', which follows our pattern. 
+ *
+ *<code>
+ *$myObject = new myClass();
+ *</code>
+ *  
+ * @param str $class_name Class to be loaded as needed
+ * @return void
+ */
+
+function __autoload($class_name) 
+{
+    if(file_exists(INCLUDE_PATH . $class_name . '.php'))
+    {
+    	include INCLUDE_PATH . $class_name . '.php';
+	}
+}#end __autoload()
+function myerror($myFile, $myLine, $errorMsg)
+
+{
+	
+    if(defined('DEBUG') && DEBUG)
+	
+    {
+    	
+       echo "Error in file: <b>" . $myFile . "</b> on line: <b>" . $myLine . "</b><br />";
+       echo "Error Message: <b>" . $errorMsg . "</b><br />";
+       die();
+    }else{
+        echo "I'm sorry, we have encountered an error.  Would you like to buy some socks?";	
+        die();
+    }
+}
 ?>
 
--- php/users/list.php
+++ php/users/list.php
@@ -1,13 +1,33 @@
 <?php 
     require 'includes/header.php';
-
+    include 'includes/Pager.php';
     /**
     * Creates and displays a list of users along
     * with their names, phone numbers, emails, and type
     */
 
-    $conn = db_connect();
-    $data = get_user_list($conn);
+    $iConn = db_connect();
+
+    //$conn = db_connect();
+    $sql  = "SELECT * FROM user_list";
+    $output = false;
+    
+
+$myPager = new Pager(10,'',"<strong>Previous</strong>","<strong>Next</strong>",'');
+$sql = $myPager->loadSQL($sql,$iConn);  #load SQL, pass in existing connection, add offset
+
+$result = mysqli_query($iConn, $sql);
+if($result){
+    while ($row = $result->fetch_assoc()) {
+        $output[] = $row;
+    }
+}
+    //clean-up result set and connection
+    
+
+
+
+    //$data = get_user_list($conn);
 
     echo 
     '
@@ -24,34 +44,59 @@
         <!--ROWS-->
 
     ';
+if($result){
+    $numResults = mysqli_num_rows($result);
+    if(mysqli_num_rows($result) > 0)
+    {#records exist - process
+	   if($myPager->showTotal()==1){$itemz = "user";}else{$itemz = "users";}  //deal with plural
+    
+	
+        echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
 
-    foreach ($data as $d) {
-        $userName = dbOut($d['Name']);#assign variables for readability
-        $userPhone = dbOut($d['Phone']);
-        $userEmail = dbOut($d['Email Address']);
-        $userType = dbOut($d['User Type']);
-        $usrId = dbOut($d['User ID']);
+        echo "<br />" . $myPager->showNAV() . "<br />"; # show paging nav, only if enough records	
+    
+        foreach ($result as $d) {
+            $userName = dbOut($d['Name']);#assign variables for readability
+            $userPhone = dbOut($d['Phone']);
+            $userEmail = dbOut($d['Email Address']);
+            $userType = dbOut($d['User Type']);
+            $usrId = dbOut($d['User ID']);
 
-        #print out table structure with data
-        echo 
-        '
-            <li>
-                <a href="detail.php?id='. $usrId . '">
-                    <ul class="inner4">
-                        <li>' . $userName . '</li>
-                        <li>' . $userPhone . '</li>
-                        <li>' . $userEmail . '</li>
-                        <li>' . $userType . '</li>
-                    </ul>
-                </a>
-            </li>
-        ';
-    }#end foreach
-    echo 
-    '
-        </ul>    
-        <a class="button" href="add.php"><div>Create new User</div></a>
-    ';
+            #print out table structure with data
+            echo 
+            '
+                <li>
+                    <a href="detail.php?id='. $usrId . '">
+                        <ul class="inner4">
+                            <li>' . $userName . '</li>
+                            <li>' . $userPhone . '</li>
+                            <li>' . $userEmail . '</li>
+                            <li>' . $userType . '</li>
+                        </ul>
+                    </a>
+                </li>
+            ';
+        }#end foreach
+    
+        if($numResults>9){
+            echo "<br />" . $myPager->showNAV() . "<br />"; # show paging nav, only if enough records	
+        }
+        mysqli_free_result($result);
+        mysqli_close($iConn);
+    }else{#no records
+        echo "<div align=center>What! No users?  There must be a mistake!!</div>";	
+    }
+    
+    
+}else{#no records
+    echo "<div align=center>What! No users?  There must be a mistake!!</div>";	
+}
+    
+echo 
+'
+    </ul>    
+    <a class="button" href="add.php"><div>Create new User</div></a>
+';
 
-    render_footer();
+render_footer();
 ?>
